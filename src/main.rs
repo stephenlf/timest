30c6075fa03cb5144d4f030365687e40cf0ebe60
stephenlf-ems <sfunk@enrollmentmanagementservices.com>
2023-11-18 09:39:07 -0500

    Progress towards fancy report

--- src/main.rs
+++ src/main.rs
@@ -77,30 +77,50 @@ fn main() {
 fn report_cmd(conn: sqlite::Connection, args: ReportArgs) {
     match args.report_style {
         ReportStyle::Simple => simple_report(&conn).unwrap(),
-        _ => todo!(),
+        ReportStyle::Fancy => fancy_report(&conn).unwrap(),
     }
 }
 
 const SQL_TODAYS_CLOCK: &str = "
     SELECT * FROM times
     WHERE Date(timestamp) = DATE('now')
+    ORDER BY timestamp
 ";
+
+fn fancy_report(conn: &sqlite::Connection) -> Result<(), anyhow::Error> {
+    let mut stmt = conn.prepare(SQL_TODAYS_CLOCK);
+
+    Ok(())
+}
 
 fn simple_report(conn: &sqlite::Connection) -> Result<(), anyhow::Error> {
     let mut stmt = conn.prepare(SQL_TODAYS_CLOCK)?;
-    println!("=======TODAY'S TIMESHEET=======");
+    println!("====TODAY'S TIMESHEET====");
+    println!("->>    {}\n", &sql_date(conn)?);
+    println!(" ________________________");
+
     for (i, row) in stmt.iter().enumerate() {
         if let Ok(mut result) = row {
             let timestamp: String = result.take(1).try_into().unwrap();
-            let (timestamp, _) = NaiveDateTime::parse_and_remainder(&timestamp, "%Y-%m-%d %H:%M:%S.").unwrap();
+            let (timestamp, _) = NaiveDateTime::parse_and_remainder(&timestamp, "%Y-%m-%d %H:%M:%S").unwrap();
             let io: String = result.take(2).try_into().unwrap();
-            println!("{i}  |  {timestamp} | {io}");
+            println!("|  {i}  |  {}  |  {io}  |", timestamp.time());
         } else {
             println!("{i}  |  Bad row 💀");
         }
     }
     Ok(())
+}
 
+fn sql_date(conn: &sqlite::Connection) -> Result<NaiveDate, anyhow::Error> {
+    let date: String = conn.prepare("SELECT DATE()")?
+        .iter()
+        .next()
+        .expect("Expect at least 1 row")?
+        .take(0)
+        .try_into()?;
+    let date = NaiveDate::parse_from_str(&date, "%Y-%m-%d")?;
+    Ok(date)
 }
 
 fn clock_cmd(conn: sqlite::Connection, args: ClockArgs) {
